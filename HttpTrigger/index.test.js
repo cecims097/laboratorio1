71f2a7d8bdfc8297478b5296284f170f63234b54
cecims097 <cms097@hotmail.com>
2021-10-13 21:20:54 -0500

    pruebas de rendimiento

--- HttpTrigger/index.test.js
+++ HttpTrigger/index.test.js
@@ -5,8 +5,12 @@ test('Http trigger example' , async() => {
     const request= {
         query: { name: 'ceci' }
 };
-
+var iterations = 1000000;
+console.time('FUNCTION #1');
+for (var i = 0; i < iterations; i++) {
     await httpFunction(context,request);
+}
+    console.timeEnd('FUNCTION #1');
     expect(context.res.body).toEqual('Welcome, ceci');
-    expect(context.log.mock.calls.length).toBe(1);
+    expect(context.log.mock.calls.length).toBe(1000000);
 });
